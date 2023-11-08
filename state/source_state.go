21f2004d003bf0267758557b6a6761443c4e4f23
Gavin Zhao <git@gzgz.dev>
2023-11-08 16:25:20 -0500

    feat(state): implement loading of source state
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

--- state/source_state.go
+++ state/source_state.go
@@ -1,8 +1,22 @@
 package state
 
 import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"path/filepath"
+	"slices"
+	"sync"
+
 	"github.com/GZGavinZhao/autobuild/common"
+	"github.com/GZGavinZhao/autobuild/config"
+	"github.com/GZGavinZhao/autobuild/utils"
+	"github.com/charlievieth/fastwalk"
 	"github.com/dominikbraun/graph"
+)
+
+var (
+	badPackages = [...]string{"haskell-http-client-tls"}
 )
 
 type SourceState struct {
@@ -32,10 +46,6 @@ func (s *SourceState) BuildGraph() {
 
 }
 
-func LoadSource(path string) (state SourceState, err error) {
-	return
-}
-
 func (cur *SourceState) Changed(old *State) (res []Diff) {
 	for idx, pkg := range cur.packages {
 		oldIdx, found := (*old).NameToSrcIdx()[pkg.Name]
@@ -60,6 +70,64 @@ func (cur *SourceState) Changed(old *State) (res []Diff) {
 				OldVer:    pkg.Version,
 			})
 		}
+	}
+
+	return
+}
+
+func LoadSource(path string) (state SourceState, err error) {
+	walkConf := fastwalk.Config{
+		Follow: false,
+	}
+	var mutex sync.Mutex
+
+	err = fastwalk.Walk(&walkConf, path, func(path string, d fs.DirEntry, err error) error {
+		// err = filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+		if !d.IsDir() {
+			return nil
+		}
+
+		// Some hard-coded problematic packages
+		if slices.Contains(badPackages[:], filepath.Base(path)) {
+			return nil
+		}
+
+		cfgFile := filepath.Join(path, "autobuild.yml")
+		if utils.FileExists(cfgFile) {
+			abConfig, err := config.Load(cfgFile)
+			if err != nil {
+				return errors.New(fmt.Sprintf("LoadSource: failed to load autobuild config file: %s", err))
+			}
+
+			if abConfig.Ignore {
+				return filepath.SkipDir
+			}
+		}
+
+		// TODO: handle legacy XML packages too
+		pkgFile := filepath.Join(path, "package.yml")
+		if !utils.FileExists(pkgFile) {
+			return nil
+		}
+
+		pkg, err := common.ParsePackage(path)
+		if err != nil {
+			return err
+		}
+
+		mutex.Lock()
+		state.packages = append(state.packages, pkg)
+		mutex.Unlock()
+
+		return filepath.SkipDir
+	})
+
+	if err != nil {
+		return
+	}
+
+	for idx := range state.packages {
+		state.packages[idx].Resolve(state.nameToSrcIdx)
 	}
 
 	return
